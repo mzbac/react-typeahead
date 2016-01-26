86ed0d4c5de93f90f1aece4629f1bc5f84054305
anchen.li <li.anchen.au@gmail.com>
2016-01-25 23:16:43 -0800

    select option function added

--- app.jsx
+++ app.jsx
@@ -7,9 +7,15 @@ ReactDOM.render(
     <Typeahead maxHeight="200" inputLabelName="typeahead" inputPlaceHolder="please enter!" source={[
     {name:'Dapibus ac facilisis in'},
      {name:'Morbi leo risus'},
+      {name:'Morbi leo risus'},
+       {name:'Morbi leo risus'},
+        {name:'Morbi leo risus'},
+         {name:'Morbi leo risus'},
      {name:'Vestibulum at eros'}
     ]
     }
-               displayItemName="name" />,
+               displayItemName="name"
+               selectedValue={(value)=>{console.log('selected +'+value.get('name'))}}
+    />,
     document.body
 );
--- src/Typeahead.jsx
+++ src/Typeahead.jsx
@@ -3,23 +3,33 @@ import Immutable from 'immutable';
 export class Typeahead extends React.Component {
     constructor(props) {
         super(props);
-        this.state={};
-        this.state.input='';
+        this.state = {};
+        this.state.input = '';
+        this.state.select = '';
+        this.state.selected=false;
     }
-    componentWillMount(){
-        this.state.source=Immutable.fromJS(this.props.source);
+
+    componentWillMount() {
+        this.state.source = Immutable.fromJS(this.props.source);
     }
+
     render() {
-        if (this.state.source&&this.state.input)
-            var options = this.state.source.filter((x) =>{
-             return (x.get(this.props.displayItemName).toUpperCase().indexOf(this.state.input.toUpperCase()) !==-1)
+        var that =this;
+        if (this.state.source && this.state.input&&!this.state.selected)
+            var options = this.state.source.filter((x) => {
+                return (x.get(this.props.displayItemName).toUpperCase().indexOf(this.state.input.toUpperCase()) !== -1)
             }).map((value)=> {
-                return <a className="list-group-item">{value.get(this.props.displayItemName)}</a>
+                return <a className="list-group-item" onClick={this.select.bind(this,value)}>{value.get(this.props.displayItemName)}</a>
             })
         return (<div>
             <div className="form-group" style={{marginBottom:0}}>
                 <label>{this.props.inputLabelName}</label>
-                <input type="text" className="form-control" placeholder={this.props.inputPlaceHolder} onChange={this.onChange.bind(this)}/>
+                <input type="text" className="form-control"
+                       value={this.state.input}
+                       placeholder={this.props.inputPlaceHolder}
+                       onChange={this.onChange.bind(this)}
+                       onBlur={this.onBlur.bind(this)}
+                />
             </div>
             <div className="list-group" style={{maxHeight: this.props.maxHeight,
                                             overflowY:'auto' }}>
@@ -28,7 +38,17 @@ export class Typeahead extends React.Component {
         </div>);
 
     }
-    onChange(event){
-        this.setState({input:event.target.value});
+
+    onChange(event) {
+        this.setState({input: event.target.value,selected:false});
+    }
+
+    onBlur(event) {
+        //if (event.target.value !== this.state.select)
+        //    this.setState({input: ''});
+    }
+    select(value){
+        this.setState({input:value.get(this.props.displayItemName),select: value.get(this.props.displayItemName),selected:true});
+        this.props.selectedValue(value);
     }
 }
